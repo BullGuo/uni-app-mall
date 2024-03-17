ba65237d5fe175b531e087008e581fc8b1fdb407
jiangguo <1521323018@qq.com>
2024-03-17 21:24:42 +0800

    feat: 新增猜你喜欢组件

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,6 +1,10 @@
 import 'vue'
+import ComGuess from '@/components/com-guess.vue'
 declare module 'vue' {
   export interface GlobalComponents {
     ComSwiper: typeof import('@/components/com-swiper.vue')['default']
+    ComGuess: typeof ComGuess
   }
 }
+
+export type ComGuessInstance = InstanceType<typeof ComGuess>
