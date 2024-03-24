c40b007f3d501323af82dd2849878263e05c153c
jiangguo <1521323018@qq.com>
2024-03-24 21:13:36 +0800

    fix: 解决订单提交页数量展示异常的问题

--- src/pagesOrder/create/create.vue
+++ src/pagesOrder/create/create.vue
@@ -105,7 +105,7 @@ async function handleOrderSubmit() {
             <view class="pay-price symbol">{{ item.payPrice }}</view>
             <view class="price symbol">{{ item.price }}</view>
           </view>
-          <view class="count">{{ item.count }}</view>
+          <view class="count">x{{ item.count }}</view>
         </view>
       </navigator>
     </view>
